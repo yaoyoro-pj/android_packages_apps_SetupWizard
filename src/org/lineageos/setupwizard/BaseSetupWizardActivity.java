ceda95176eadd199421cc32482e96dcc082be504
Bruno Martins <bgcngm@gmail.com>
2024-04-27 14:09:55 +0100

    BaseSetupWizardActivity: Handle NPE when WiFi service is unavailable
    Do not let SetupWizard crash in case the WiFi service is not available,
    which can happen in case of devkits without built-in WiFi support.
    
    Change-Id: Iba72e941b5e4995e865c70cb6c2b6be9a721addc

--- src/org/lineageos/setupwizard/BaseSetupWizardActivity.java
+++ src/org/lineageos/setupwizard/BaseSetupWizardActivity.java
@@ -287,7 +287,7 @@ public abstract class BaseSetupWizardActivity extends AppCompatActivity implemen
 
     protected final boolean tryEnablingWifi() {
         WifiManager wifiManager = getSystemService(WifiManager.class);
-        return wifiManager.setWifiEnabled(true);
+        return wifiManager != null && wifiManager.setWifiEnabled(true);
     }
 
     private boolean isFirstRun() {
